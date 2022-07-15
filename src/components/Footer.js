9172c0522ecef16986e7029644ba667e8dec624d
vlathong <105715507+vlathong@users.noreply.github.com>
2022-07-15 23:36:54 +0700

    /

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import TwitterIcon from '@mui/icons-material/Twitter';
-
+import "../styles/Footer.css";
 
 const Footer = () => {
     return (
